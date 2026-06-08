09cf1ce350c4aed6d342e851ce8c0947fe6dfc95
xiaobin3031 <xiaobin1124@gmail.com>
2026-06-08 23:44:46 +0800

    调整配置

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -29,6 +30,10 @@ export default defineConfig(({ mode }) => {
       sourcemap: false, // 是否生成 source map
       minify: 'esbuild', // 或 'terser'
       rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          login: resolve(__dirname, 'login.html')
+        },
         output: {
           // 控制输出文件名格式
           entryFileNames: 'assets/[name].[hash].js',
